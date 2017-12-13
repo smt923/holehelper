7aea5fd0ca30a1300c6ce174f59d1f4f94b68d1a
smt923 <smtea923@gmail.com>
2017-12-13 05:42:00 +0000

    Update format and logs

--- main.go
+++ main.go
@@ -9,13 +9,17 @@ import (
 )
 
 func main() {
+	now := time.Now().UTC()
+	fmt.Printf("[%s]: %s\n", now.Format("03:04 PM"), "Starting, simply select all (Ctrl-A) on the Wormhole description and copy it (Ctrl-C), this log is in EVE Time.")
 	for {
-		clipboardLoop()
+		// Refresh our time each loop - is there a better way to do this?
+		now = time.Now().UTC()
+		clipboardLoop(now)
 		time.Sleep(500 * time.Millisecond)
 	}
 }
 
-func clipboardLoop() {
+func clipboardLoop(theTime time.Time) {
 	cb, err := clipboard.ReadAll()
 	// Sometimes the handle will just give out, there's little harm in restarting our loop with the delay
 	if err != nil {
@@ -32,40 +36,40 @@ func clipboardLoop() {
 		return
 	}
 
-	finishedString += checkLife(lines[4]) + " / "
-	finishedString += checkMass(lines[5]) + " / "
-	finishedString += checkShips(lines[6])
+	finishedString += "LIFE: " + checkLife(lines[4]) + " | "
+	finishedString += "MASS: " + checkMass(lines[5]) + " | "
+	finishedString += "SIZE: " + checkShips(lines[6])
 
 	clipboard.WriteAll(finishedString)
-	now := time.Now().UTC()
-	fmt.Printf("[%s]: %s\n", now.Format("03:04 PM"), finishedString)
+
+	fmt.Printf("[%s]: %s\n", theTime.Format("03:04 PM"), finishedString)
 }
 
 func checkLife(line string) string {
 	if strings.Contains(line, "not yet begun") {
-		return "Not yet"
+		return ">24 hrs"
 	}
 	if strings.Contains(line, "beginning to decay") {
-		return "Beginning"
+		return "4-24 hrs"
 	}
 	if strings.Contains(line, "reaching the end") {
-		return "Reaching the end"
+		return "<4hrs"
 	}
 	if strings.Contains(line, "on the verge") {
-		return "On the verge"
+		return "<15m"
 	}
 	return ""
 }
 
 func checkMass(line string) string {
 	if strings.Contains(line, "not yet") {
-		return "Not yet (> 50%)"
+		return ">50%"
 	}
 	if strings.Contains(line, "not to a critical degree") {
-		return "Not to critical degree (10-50%)"
+		return "10-50%"
 	}
 	if strings.Contains(line, "stability critically disrupted") {
-		return "Critical (< 10%)"
+		return "<10%"
 	}
 	return ""
 }
@@ -75,7 +79,7 @@ func checkShips(line string) string {
 		return "Very Large"
 	}
 	if strings.Contains(line, "Larger ships") {
-		return "Larger"
+		return "Large"
 	}
 	if strings.Contains(line, "medium size") {
 		return "Medium"
